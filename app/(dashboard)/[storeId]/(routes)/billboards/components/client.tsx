15cdc1c4eb3cf1ec8969cc4d372c7365eae6ab2b
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-08-18 18:30:46 +0200

    billboards added

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+export const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Billboards (0)" description="Manage store" />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
